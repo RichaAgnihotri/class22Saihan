bd71a769b8db5060084f3910844797398106fba6
RichaAgnihotri <68606897+RichaAgnihotri@users.noreply.github.com>
2023-01-19 04:30:37 +0530

    Update cannonBall.js

--- js/cannonBall.js
+++ js/cannonBall.js
@@ -1,54 +1,52 @@
-class CannonBall{
-    constructor(x,y){
-        var options = {
-        isStatic:true 
-        };
-        this.r = 40;
-        this.body = Bodies.circle(x,y,this.r,options);
-        this.image = loadImage("./assets/cannonball.png");    
-        this.trajectory = [];//this is creation of array to store the path of the ball
-        World.add(world,this.body);
+class CannonBall {
+  constructor(x, y) {
+    var options = {
+      isStatic: true
+    };
+    this.r = 30;
+    this.body = Bodies.circle(x, y, this.r, options);
+    this.image = loadImage("./assets/cannonball.png");
+    this.trajectory = [];
+    World.add(world, this.body);
+  }
+
+  remove(index) {
+    Matter.Body.setVelocity(this.body, { x: 0, y: 0 });
+
+    setTimeout(() => {
+      Matter.World.remove(world, this.body);
+      delete balls[index];
+    }, 1000);
+  }
+
+  shoot() {
+     var newAngle = cannon.angle - 28;
+    newAngle = newAngle *(3.14/180)
+    var velocity = p5.Vector.fromAngle(newAngle);
+    velocity.mult(0.5);
+    Matter.Body.setStatic(this.body, false);
+    Matter.Body.setVelocity(this.body, {
+      x: velocity.x *(180/3.14), y: velocity.y * (180/3.14)});
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.r, this.r);
+    pop();
+
+    if (this.body.velocity.x > 0 && pos.x > 10) {
+      var position = [pos.x, pos.y];
+      this.trajectory.push(position);
     }
 
-    remove(index){
-        Matter.Body.setVelocity(this.body,{x:0, y:0});
-        setTimeout(()=>{
-            Matter.World.remove(world,this.body)
-            delete balls[index];
-        },1000)
-        }
-    
-  
-  
-  
-
-    shoot(){
-        var velocity = p5.Vector.fromAngle(cannon.angle);
-        velocity.mult(20);
-       Matter.Body.setStatic(this.body, false);
-        Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y});
+    for (var i = 0; i < this.trajectory.length; i++) {
+      image(this.image, this.trajectory[i][0], this.trajectory[i][1], 5, 5);
     }
-    display(){
-        var angle = this.body.angle
-        var pos = this.body.position;
-        push();//used to implement property
-        translate(pos.x, pos.y);
-        rotate(angle);
-        imageMode(CENTER);
-        image(this.image,0,0,this.r,this.r);
-        pop();
-
-
-
-        if(this.body.velocity.x > 0 && this.body.position.x>300){
-        var position = [this.body.position.x, this.body.position.y]
-        this.trajectory.push(position);//push is used to add value in the array
-
-        }
-
-        for (var i = 0; i< this.trajectory.length;i++){
-            image(this.image, this.trajectory[i][0], this.trajectory[i][1],5,5)
-        }
-    }
-
+  }
 }
